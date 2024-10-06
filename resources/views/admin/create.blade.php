b8c3be1b876559ab061ad23d1a9fda596f41ca06
WillyDiMartino <guillermodimartinoperez@gmail.com>
2024-10-06 18:06:40 -0300

    prueba02

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -8,8 +8,35 @@
         <div class="alert alert-danger container">Hay errores en los datos ingresados</div>
         @endif
 
-        <x-forms.tinymce-editor/>
-        
+        <form action="{{ url('/admin/publicar') }}" method="post" class="container">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Título</label>
+                <input type="text" id="title" name="title" class="form-control" value="{{ old('title') }}" @error("title") aria-errormessage="error-title" @enderror>
+                @error('title')
+                <div class="text-danger" id="error-title">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="content" class="form-label">Contenido</label>
+                <textarea id="content" name="content" class="form-control" @error("content") aria-errormessage="error-content" @enderror>{{ old('content') }}</textarea>
+                @error('content')
+                <div class="text-danger" id="error-content">{{ $message }}</div>
+                @enderror
+            </div>
+            
+            <div class="mb-3">
+                <label for="release_date" class="form-label">Fecha de publicación</label>
+                <input type="date" id="release_date" name="release_date" class="form-control" value="{{ old('release_date') }}" @error("release_date") aria-errormessage="error-release_date" @enderror>
+                @error('release_date')
+                <div class="text-danger" id="error-release_date">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <button type="submit" class="btn btn-primary">Publicar</button>
+        </form>
+
     </section>
-    
+
 </x-admin_layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", [\App\Http\Controllers\HomeController::class, "index"])->name("home");
-Route::get("/noticias", [\App\Http\Controllers\ArticleController::class, "index"])->name("articles.index");
+Route::get("/noticias", [\App\Http\Controllers\ArticleController::class, "index"])->name("articles.index")->middleware("auth");
 Route::get("/suscripcion", [\App\Http\Controllers\PlanController::class, "index"])->name("plan.index");
 
 Route::get("/iniciar-sesion", [\App\Http\Controllers\AuthController::class, "login"])->name("auth.login");
